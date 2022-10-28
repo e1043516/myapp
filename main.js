7bfbb91922d9e0dfb2992b3332918d3820b2de7e
e1043516 <e1043516@u.nus.edu>
2022-10-28 10:59:45 +0800

    My first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// Import packages
+const express = require('express')
+const morgan = require('morgan')
+
+// Configure out port 
+// Check if it set in the environment variable, if not then we default to 3000
+const port = process.env.PORT || 3000
+
+//Create an instance of express application
+const app = express()
+
+//Log all request
+app.use(morgan('common'))
+
+//GET/
+app.get('/',(req,resp) => {
+    //return HTML content
+    resp.type('text/html')
+    //set the status code
+    resp.status(200)
+    //send the response
+    resp.send(`<h1>The current new time is ${new Date()} </h1>`) //when you need to substitute sth, you need to use ` instead of '
+
+})
+
+//Start our web application
+app.listen(port,() => {
+    console.info(`Starting application on port ${port} at ${new Date()}`) //note this sign, at the top left hand corner, right below ESC button
+})
